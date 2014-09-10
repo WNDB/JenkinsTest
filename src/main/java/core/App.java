3564029c78f6b6c0da309ceda2994e957de88266
Wojciech Nadgłowski <wojciech.nadglowski@docusafe.pl>
2014-09-10 15:00:39 +0200

    Test2

--- src/main/java/core/App.java
+++ src/main/java/core/App.java
@@ -7,4 +7,7 @@ public class App {
     public static void main(String args[]){
         System.out.println("**************************Projekt ruszyl**********************************************");
     }
+    public static Integer returnOne(){
+        return 1;
+    }
 }
