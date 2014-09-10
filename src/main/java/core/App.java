1f81bd23b3c3fe9f771188616bd294ade7b68c3b
Wojciech Nadgłowski <wojciech.nadglowski@docusafe.pl>
2014-09-10 14:05:39 +0200

    testCommit

---
+++ src/main/java/core/App.java
@@ -0,0 +1,10 @@
+package core;
+
+/**
+ * Created by Wojtek on 2014-09-10.
+ */
+public class App {
+    public static void main(String args[]){
+        System.out.println("**************************Projekt ruszyl**********************************************");
+    }
+}
